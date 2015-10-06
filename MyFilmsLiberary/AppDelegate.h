105e17eb67573888f39a656ccde3c37006ea1e0a
Orville Miller <s12773932@CC1308T2-DM2714.local>
2015-10-06 13:41:33 +0100

    Initial Commit

---
+++ MyFilmsLiberary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyFilmsLiberary
+//
+//  Created by Orville Miller on 06/10/2015.
+//  Copyright (c) 2015 Orville Miller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
